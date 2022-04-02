c2e3b1ff34162434a9c8279fbc47eb7b21f1ac20
mariinay <marina.jovanovic.mj@gmail.com>
2022-04-02 13:37:04 +0200

    UserData model created

---
+++ mbooks_laravel/app/Models/UserData.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserData extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'surname',
+        'email',
+        'adress',
+        'city',
+        'postal_code',
+        'phone_number',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function order()
+    {
+        return $this->hasMany(Order::class);
+    }
+}
